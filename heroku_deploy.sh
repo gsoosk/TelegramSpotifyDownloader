2c864324565573ded2fccd3118b792a881af8e6d
Farzad <farzadhabibii98@gmail.com>
2021-11-06 14:38:43 +0330

    Update heroku_deploy

--- heroku_deploy.sh
+++ heroku_deploy.sh
@@ -1,4 +1,5 @@
 heroku create
 heroku buildpacks:add --index 1 https://github.com/jonathanong/heroku-buildpack-ffmpeg-latest.git
+heroku config:set $(cat .env)
 git push heroku master
 heroku ps:scale worker=1

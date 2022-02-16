96cde5fadfc73e3ea19caccdfa395d3a17ec8205
Ndrewly <91999184+Ndrewly@users.noreply.github.com>
2022-02-16 23:08:43 +0700

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ try:
 except:
     token = os.environ['TELEGRAM_TOKEN']
 
-updater = Updater(token, use_context=True)
+updater = Updater(token=telegram_token, use_context=True)
 dispatcher = updater.dispatcher
 
 def get_single_song_handler(bot, update):
